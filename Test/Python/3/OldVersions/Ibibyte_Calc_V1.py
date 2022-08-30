ff925179c6dc11c51d924c33d5ac5ec82b3540bf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-29 22:39:55 -0700

    Ibibyte_Calc_V1.py
    An archive of the first version of the -Ibibyte calculator, written in Python 3.

---
+++ Test/Python/3/OldVersions/Ibibyte_Calc_V1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# =Ibibyte calculatoe
+# May require a 128 bit version of Python to function properly (past 2^60 for 64 bit, and past 2^30 for 32 bit)
+# Verbosity is acceptable here, as this is a beginners concept, and should be readable by even low level/non programmers
+print("-Ibibyte calculator (Python Edition)")
+print("Version 1\n")
+print("2^10 = " + 2 ** 10)
+print("2^20 = " + 2 ** 20)
+print("2^30 = " + 2 ** 30)
+print("2^40 = " + 2 ** 40)
+print("2^50 = " + 2 ** 50)
+print("2^60 = " + 2 ** 60)
+print("2^70 = " + 2 ** 70)
+print("2^80 = " + 2 ** 80)
+print("2^90 = " + 2 ** 90)
+print("2^100 = " + 2 ** 100)
+print("2^110 = " + 2 ** 110)
+print("2^120 = " + 2 ** 120)
+noMore = input("Press [ENTER] key to quit")
+# File info
+# File type: Python 3 source file (*.py)
+# File version: 1 (2022, Monday, August 29th at 10:39 pm PST)
+# Line count (including blank lines and compiler line): 25
+# End of script
